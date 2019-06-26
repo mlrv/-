eab9ddd827f02c8d6175adc1cfabccbc397ab587
Marco Levrero <marco.levrero@gmail.com>
2019-06-26 16:59:51 +0100

    Show instance for Cat TC

--- src/main/scala/sandbox/1/Cat.scala
+++ src/main/scala/sandbox/1/Cat.scala
@@ -1,6 +1,8 @@
 package cat
 
 import printable._
+import cats.Show
+import cats.instances.string._
 
 final case class Cat(name: String, age: Int, color: String)
 
@@ -8,5 +10,10 @@ object Cat {
   implicit val printableCat: Printable[Cat] =
     new Printable[Cat] {
       def format(v: Cat): String = s"${v.name} is a ${v.age} year-old ${v.color} cat"
+    }
+
+  implicit val shownCat: Show[Cat] =
+    new Show[Cat] {
+      def show(v: Cat): String = s"${v.name} is a ${v.age} year-old ${v.color} cat"
     }
 }
