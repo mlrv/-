f00653a00370b70ae07e43d4f31666d9c9a452c3
Marco Levrero <marco.levrero@gmail.com>
2019-07-02 16:16:34 +0100

    Generic adder

--- src/main/scala/sandbox/2/Adder.scala
+++ src/main/scala/sandbox/2/Adder.scala
@@ -2,9 +2,13 @@ package adder
 
 import cats.Monoid
 import cats.instances.int._
+import cats.instances.option._
 import cats.syntax.semigroup._ // for |+|
 
 object Adder {
-  def add(items: List[Int]): Int =
-    items.foldLeft(Monoid[Int].empty)(_ |+| _)
+  def add[A](items: List[A])(implicit monoid: Monoid[A]): A =
+    items.foldLeft(monoid.empty)(_ |+| _)
+
+  def optionAdd(items: List[Option[Int]]): Option[Int] =
+    add(items)(Monoid[Option[Int]])
 }
