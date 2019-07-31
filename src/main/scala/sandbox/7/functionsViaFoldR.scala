96f398335c539c90f0524436c0970b816e22e495
Marco Levrero <marco.levrero@gmail.com>
2019-07-31 21:13:28 +0100

    More functions via foldRight

--- src/main/scala/sandbox/7/functionsViaFoldR.scala
+++ src/main/scala/sandbox/7/functionsViaFoldR.scala
@@ -6,7 +6,13 @@ object FunctionsViaFoldR {
   implicit def listToEnrichedList[A](l: List[A]) = new EnrichedList(l)
 
   case class EnrichedList[A](targetList: List[A]) {
-    def mapR[B](f: A => B): EnrichedList[B] =
+    def mapR[B](f: A => B): List[B] =
       targetList.foldRight(List.empty[B])((i, a) => f(i) :: a)
+    
+    def filterR(f: A => Boolean): List[A] =
+      targetList.foldRight(List.empty[A])((i, a) => if (f(i)) i :: a else a)
+    
+    def flatMapR[B](f: A => List[B]): List[B] =
+      targetList.foldRight(List.empty[B])((i, a) => f(i) ::: a)
   }
 }
